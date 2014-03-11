8d0c08ae7fe0f9b59451c8f2f78622611c1395c8
CamBurris <cameron.burris1@gmail.com>
2014-03-11 15:07:13 -0500

    Pagination to project list
    Pagination added to project list, favicon implemented.

--- mysite/project/forms.py
+++ mysite/project/forms.py
@@ -1,7 +1,10 @@
 from django import forms
 from project.models import Project
+#from taggit.forms import TagField
 
 class ProjectForm(forms.ModelForm):
 	class Meta:
 		model = Project
 		fields = ['project_name', 'teacher_name', 'subject', 'outlet', 'abstract']
+	
+	#tags = TagField()
--- mysite/project/views.py
+++ mysite/project/views.py
@@ -3,11 +3,24 @@ from project.models import Project
 from project.forms import ProjectForm
 from django.http import HttpResponseRedirect
 from django.core.context_processors import csrf
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 # Create your views here.
 def index(request):
-	latest_project_list = Project.objects.order_by('-id')[:5]
-	return render(request, 'project/index.html', {'latest_project_list': latest_project_list})
+	#latest_project_list = Project.objects.order_by('-id')[:5]
+	#return render(request, 'project/index.html', {'latest_project_list': latest_project_list})
+	project_list = Project.objects.all()
+	paginator = Paginator(project_list, 5)
+	page = request.GET.get('page')
+	
+	try:
+		projects = paginator.page(page)
+	except PageNotAnInteger:
+		projects = paginator.page(1)
+	except EmptyPage:
+		projects = paginator.page(paginator.num_pages)
+		
+	return render(request, 'project/index.html', {'latest_project_list' : projects})
 	
 def detail(request, project_id):
 	project = get_object_or_404(Project, pk=project_id)
